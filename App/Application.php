17c287b08da3ded265dfbf26e7e2e5624146509a
bogdasari4 <tipbodyaone@gmail.com>
2024-03-28 10:49:50 +0300

    Added comments to the App class

--- App/Application.php
+++ App/Application.php
@@ -1,6 +1,16 @@
 <?php
+/**
+ * MyOpenMuWeb
+ * @see https://github.com/bogdasari4/MyOpenMuWeb
+ */
+
 class App
 {
+    /**
+     * The initial class of the engine. We declare the first constants and set the php settings.
+     * 
+     * @author Bogdan Reva <tip-bodya@yandex.com>
+     */
     public function getLoader()
     {
         /**
@@ -9,10 +19,19 @@ class App
          * Set '0' to turn off all error reporting.
          * Set 'E_ALL & ~E_NOTICE' to report all errors except E_NOTICE.
          * Set 'E_ALL' to report all PHP errors.
+         * 
+         * @see https://www.php.net/manual/function.error-reporting.php
          */
         error_reporting(E_ALL);
 
+        /**
+         * @see https://www.php.net/manual/function.ini-set.php
+         */
         @ini_set('default_charset', 'utf-8');
+
+        /**
+         * @see https://www.php.net/manual/function.ob-start.php
+         */
         @ob_start();
 
         define('__HTTP_HOST', isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'CLI');
@@ -23,7 +42,7 @@ class App
         define('__BASE_URL_ALT', __SERVER_PROTOCOL . __HTTP_HOST);
         define('__ACTUAL_URL', __SERVER_PROTOCOL . __HTTP_HOST . $_SERVER['REQUEST_URI']);
 
-        $classMap = [
+        $applicationClassMap = [
             null => 'App/Packets/autoload.php',
             '\\App\\Alert' => 'App/Alert.php',
             '\\App\\Assistant' => 'App/Assistant.php',
@@ -31,7 +50,7 @@ class App
             '\\App\\Core' => 'App/Core/Core.php'
         ];
 
-        foreach ($classMap as $namespace => $file) {
+        foreach ($applicationClassMap as $namespace => $file) {
             if (file_exists($file)) {
                 require_once($file);
                 if($namespace == '\\App\\Core') {
